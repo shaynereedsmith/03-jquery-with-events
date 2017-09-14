bb6f77c14f68e4184ee068b309781f89b94f158f
shaynereedsmith <shaynereedsmith@yahoo.com>
2017-09-14 09:40:48 -0700

    Made all the stuff do the things.

--- cody-shayne/scripts/articleView.js
+++ cody-shayne/scripts/articleView.js
@@ -37,18 +37,15 @@ articleView.handleAuthorFilter = function() {
     //         defining. "$(this)" is using jQuery to select that element, so we can chain jQuery methods
     //         onto it.
     if ($(this).val()) {
-      // TODO: If the select box was changed to an option that has a value, we need to hide all the articles,
+      // DONE: If the select box was changed to an option that has a value, we need to hide all the articles,
       //       and then show just the ones that match for the author that was selected.
       //       Use an "attribute selector" to find those articles, and fade them in for the reader.
       $('article').css('display','none');
-      $('Kevin Bacon').toggle('.display');
-      console.log(this.value)
-
-
-
-
+      $('article[data-author="' + this.value + '"]').show();
     } else {
-      // TODO: If the select box was changed to an option that is blank, we should
+      $('article').show();
+      $('.template').hide();
+      // DONE: If the select box was changed to an option that is blank, we should
       //       show all the articles, except the one article we are using as a template.
 
     }
@@ -57,22 +54,37 @@ articleView.handleAuthorFilter = function() {
 };
 
 articleView.handleCategoryFilter = function() {
-  // TODO: Just like we do for #author-filter above, we should handle change events on the #category-filter element.
+  // DONE: Just like we do for #author-filter above, we should handle change events on the #category-filter element.
   //       When an option with a value is selected, hide all the articles, then reveal the matches.
   //       When the blank (default) option is selected, show all the articles, except for the template.
   //       Be sure to reset the #author-filter while you are at it!
-
+  $('#category-filter').on('change', function(){
+    if ($(this).val()) {
+      $('author-filter').val('');
+      $('article').css('display', 'none');
+      $('article[data-category="' + this.value + '"]').show();
+    } else {
+      $('article').show();
+      $('.template').hide();
+    }
+  })
 };
 
 articleView.handleMainNav = function() {
-  // TODO: Add an event handler to .main-nav elements that will power the Tabs feature.
+  // DONE: Add an event handler to .main-nav elements that will power the Tabs feature.
   //       Clicking any .tab element should hide all the .tab-content sections, and then reveal the
   //       single .tab-content section that is associated with the clicked .tab element.
   //       So: You need to dynamically build a selector string with the correct ID, based on the
   //       data available to you on the .tab element that was clicked.
-
-
-  $('.main-nav .tab:first').click(); // Let's now trigger a click on the first .tab element, to set up the page.
+  $('.icon-home').click(function(){
+    $('section#articles').show();
+    $('section#about').hide();
+  })
+  $('.icon-address-book').click(function(){
+    $('section#about').show();
+    $('section#articles').hide();
+  })
+  $('.main-nav .tab').click(); // Let's now trigger a click on the first .tab element, to set up the page.
 };
 
 articleView.setTeasers = function() {
@@ -83,7 +95,11 @@ articleView.setTeasers = function() {
   //       "Read On" link once it has been clicked. Be sure to prevent the default link-click action!
   //       Ideally, we'd attach this as just 1 event handler on the #articles section, and let it
   //       process any .read-on clicks that happen within child nodes.
-
+  $('.read-on').click(function(event) {
+    event.preventDefault();
+    $(event.target).parent().children().children().show();
+    $(event.target).html('Show Less &larr;');
+  });
   // STRETCH GOAl!: change the 'Read On' link to 'Show Less'
 
 };
@@ -92,4 +108,7 @@ articleView.setTeasers = function() {
 $(document).ready(function() {
   articleView.populateFilters();
   articleView.handleAuthorFilter();
+  articleView.handleCategoryFilter();
+  articleView.handleMainNav();
+  articleView.setTeasers();
 })
