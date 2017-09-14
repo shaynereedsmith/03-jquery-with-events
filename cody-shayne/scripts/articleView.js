75795108ca69c66969acca4c39870905bdea11c1
shaynereedsmith <shaynereedsmith@yahoo.com>
2017-09-13 17:28:58 -0700

    got dropdowns to display.

--- cody-shayne/scripts/articleView.js
+++ cody-shayne/scripts/articleView.js
@@ -92,5 +92,5 @@ articleView.setTeasers = function() {
 
 // TODO: Call all of the above functions, once we are sure the DOM is ready.
 $(document).ready(function() {
-
+  articleView.populateFilters();
 })
